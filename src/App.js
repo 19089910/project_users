eb5c9e981333ce88819263b9e15f261e1759201e
Lucas Barbosa <lucasbarbosaferreira.lb@gmail.com>
2023-05-26 19:44:03 -0400

    chore: create user card

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import People from './assets/people-talk.svg'
 import Arrow from './assets/arrow.svg'
+import Trash from './assets/trash.svg'
 
 import {
   Container,
@@ -10,9 +11,16 @@ import {
   InputLabel,
   Input,
   Button,
+  User,
 } from "./styles";
 
 function App() {
+  //aqui se coloca o javascript
+  const users = [
+    { id: Math.random(), name: "lucas", age: "22"},
+    { id: Math.random(), name: "levi", age: "19"}
+  ]
+
   return (
     <Container>
       <Image alt="logo-imagem-people" src={People}/>
@@ -26,9 +34,20 @@ function App() {
         <Input placeholder="Idade" />
 
         <Button>Cadastrar<img alt="seta" src={Arrow}></img></Button>
+
+        <ul>
+          {users.map((user) =>( 
+            <User key={user.id}>
+              <p>{user.name}</p><p>{user.age}</p>
+              <button><img alt="lata-de-lixo" src={Trash}></img></button>
+            </User>
+          ))}
+        </ul>
       </ContainerItens>
     </Container>
   );
 }
+
+
 
 export default App;
--- src/styles.js
+++ src/styles.js
@@ -46,7 +46,6 @@ export const H1 = styled.h1`
     text-align: center;
 
     color: #FFFFFF;
-
 `;
 
 export const InputLabel = styled.p`
@@ -108,4 +107,56 @@ export const Button = styled.button`
     justify-content: center;
     align-items: center;
     gap: 20px;
+
+    &:hover{
+        opacity: 0.8;
+    }
+    
+    &:active{
+        opacity: 0.5;
+    }
+`;
+
+export const User = styled.li`
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+    margin-top: 20px;
+
+    background: rgba(255, 255, 255, 0.25);
+    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+    border-radius: 14px;
+
+    width: 342px;
+    height: 58px;
+
+    //RETIRAR PADRAO:
+    border: none;
+    outline: none;
+
+    //LETRA INTERNA:
+    padding-left: 25px;
+
+    font-family: 'Roboto';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 20 px;
+    line-height: 28px;
+
+    color: #FFFFFF;
+
+    p {
+        font-weight: normal;
+        font-style: normal;
+        font-size: 20;
+        line-height: 28px;
+
+        color: #FFFFFF;
+    }
+
+    button {
+        background: none;
+        border: none;
+        cursor: pointer;
+    }
 `;
